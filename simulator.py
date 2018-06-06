5554c7d1743f2a66350bbc6c3a21045d782baf15
Sami <aaa0050@gmail.com>
2018-06-06 16:53:12 +0200

    [wip] implementation of the Ray Tracing routine /!\ Now python 2.7

--- simulator.py
+++ simulator.py
@@ -1,4 +1,4 @@
-#! /usr/bin/python3 -u
+#! /usr/bin/python2.7 -u
 # -*- coding: utf-8 -*-
 
 """Wireless UAV simulator.
@@ -26,16 +26,22 @@ Options:
     -h, --help
 """
 import logging
+import tempfile
+from rayTracingWrapper import CloudRT
 
 
-def main(logger, drone, user, bs, mapEnv):
+def main(logger, drone, user, bs):
 
-    while True:
-        antennas = rayTracing(drone, user, bs, mapEnv)
+    RESULT_DIR = tempfile.mkdtemp()
 
-        drone.updateAntenna(*antennas)
+    rt = CloudRT(RESULT_DIR)
+    rt.setTxPose(drone.x, drone.y, drone.z, drone.u, drone.v, drone.w)
+    rt.setTxPose(user.x, user.y, user.z, user.u, user.v, user.w)
 
-        drone.routine()
+    # while True:
+    rayTracing(rt, drone, user, bs)
+
+    drone.routine()
 
 
 def args():
@@ -43,60 +49,72 @@ def args():
     logging.basicConfig(level=logging.DEBUG)
     logger = logging.getLogger(__name__)
 
-    user = Terminal(logger, 0, 0, 0, 0)
-    bs = Terminal(logger, 0, 0, 0, 0)
-    drone = Drone(logger, 0, 0, 120, 0)
-    mapEnv = 0
+    # TODO cleaner way to handle initial value
+    user = Terminal(logger, 538, 459, 2, 0, 0, 0)
+    bs = Terminal(logger, 96, 69, 2, 0, 0, 0)
+    drone = Drone(logger, 50, 200, 40, 0, 180, 0)
 
-    return [logger, drone, user, bs, mapEnv]
+    return [logger, drone, user, bs]
 
 
-def rayTracing(drone, user, bs, mapEnv):
+def rayTracing(rt, drone, user, bs):
     """Compute received signal on the antenna for a given situation."""
-    return [Antenna()] * 4
+
+    # User to Drone
+    rt.setTxPose(user.x, user.y, user.z, user.u, user.v, user.w)
+    rt.setRxPose(drone.x, drone.y, drone.z, drone.u, drone.v, drone.w)
+
+    CTF_Im = rt.simulate()
+    drone.ant[0].CTF_Im = CTF_Im
+
+    # Base-station to Drone
+    rt.setTxPose(bs.x, bs.y, bs.z, bs.u, bs.v, bs.w)
+    rt.setRxPose(drone.x, drone.y, drone.z, drone.u, drone.v, drone.w)
+
+    CTF_Im = rt.simulate()
+    drone.ant[1].CTF_Im = CTF_Im
 
 
 class Antenna(object):
     """docstring for Antenna"""
     def __init__(self):
-        pass
+        self.CTF_Im = 0
+        self.CTF_Re = 0
 
 
 class Terminal(object):
     """docstring for Terminal"""
-    def __init__(self, logger, x, y, z, yaw):
+    def __init__(self, logger, x, y, z, u, v, w):
         # super(Terminal, self).__init__()
         self.logger = logger
         self.x = x
         self.y = y
         self.z = z
-        self.yaw = yaw
+        self.u = u
+        self.v = v
+        self.w = w
 
-        self.antennas = [Antenna()]
+        self.ant = [Antenna()]
 
     def _addAntenna(self):
-        self.antennas.append(Antenna())
+        self.ant.append(Antenna())
 
     def updatePos(self, x, y):
         pass
 
-    def updateAntenna(self, *antennas):
-        for idx, ant in zip(range(len(antennas)), antennas):
-            self.antennas[idx] = ant
-
 
 class Drone(Terminal):
     """docstring for Drone"""
-    def __init__(self, logger, x, y, z, yaw):
-        Terminal.__init__(self, logger, x, y, z, yaw)
+    def __init__(self, logger, x, y, z, u, v, w):
+        Terminal.__init__(self, logger, x, y, z, u, v, w)
 
-        self._addAntenna()
-        self._addAntenna()
         self._addAntenna()
 
     def routine(self):
         self.logger.debug('Hello from drone routine')
-        pass
+        self.logger.info('Drone is at (%d, %d, %d)', self.x, self.y, self.z)
+        self.logger.info('User to drone: ant0 = ' + str(self.ant[0].CTF_Im[0]))
+        self.logger.info('User to drone: ant1 = ' + str(self.ant[1].CTF_Im[0]))
 
 
 if __name__ == '__main__':
